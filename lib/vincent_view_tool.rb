35688b2dce5ae5896530e834138d500d55724b2d
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 17:32:01 +0000

    copyright to footer of portfolio

---
+++ lib/vincent_view_tool.rb
@@ -0,0 +1,6 @@
+require "vincent_view_tool/version"
+require "vincent_view_tool/renderer"
+
+module DevcampViewTool
+  # Your code goes here...
+end
---
+++ lib/vincent_view_tool/version.rb
@@ -0,0 +1,3 @@
+module VincentViewTool
+  VERSION = "0.1.0"
+end
